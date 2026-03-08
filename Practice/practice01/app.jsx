2ce2558200b5361bde7d91869876772838ebcdc2
Ketan Shetge <ketanshetgedev@gmail.com>
2026-03-08 18:32:05 +0530

    add Practice 01 - React and Vite complete setup

---
+++ Practice/practice01/app.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const element = <h1> Hello React, My name is Ketan !!</h1>
+
+ReactDOM.createRoot(document.getElementById("root")).render(element);
+
