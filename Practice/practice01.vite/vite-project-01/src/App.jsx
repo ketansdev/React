d9c2c2d17c57260caaa89fa81f18eff0e52fbe3b
Ketan Shetge <ketanshetgedev@gmail.com>
2026-03-10 06:01:19 +0530

    add Practice 01 - complete Vite setup

---
+++ Practice/practice01.vite/vite-project-01/src/App.jsx
@@ -0,0 +1,20 @@
+
+export default function App(){
+    return(
+        <h1>Hello Coder Army</h1>
+    )
+}
+
+
+export function Ketan(){
+    return(
+        <h2>My Name is Ketan</h2>
+    )
+}
+
+
+export function Swapnil(){
+    return(
+        <h3>I am Swapnil, Ketan is my friend</h3>
+    )
+}
