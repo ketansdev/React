8c049e300b7348208c04cbe90e153e5a274f9cf5
Ketan Shetge <ketanshetgedev@gmail.com>
2026-04-10 14:18:02 +0530

    Module02 - lecture03: Hoisting and TDZ

---
+++ React Course/Module-02/lecture03.js
@@ -0,0 +1,43 @@
+// var a;
+// console.log(a);
+// a = 10;
+
+// console.log(a)
+
+
+
+
+// let
+
+// console.log(b);
+
+// let b = 20;
+// console.log(b)
+
+
+// const 
+
+// console.log(c);
+
+// const c = true;
+
+
+
+
+// Hoisting Function
+
+// function greet(){
+//     console.log("Hello JS");
+// }
+
+// greet();
+
+
+
+// Function expression Hoisting 
+
+greet();
+
+var greet = function(){
+    console.log("Hello JS")
+}
