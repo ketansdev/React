2797deae3695a460b9429c9a63e24e97deff558b
Ketan Shetge <ketanshetgedev@gmail.com>
2026-04-10 10:19:46 +0530

    Build React app from Scratch

---
+++ React Course/Module-01/app01/my app/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
+
+const myFirstElement = <h1>My First Web Page in React</h1>
+
+ReactDOM.createRoot(document.getElementById("root")).render(myFirstElement);
