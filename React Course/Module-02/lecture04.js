8bb014afe6d53d4cf43b4a4fec21eca215164e91
Ketan Shetge <ketanshetgedev@gmail.com>
2026-04-11 04:54:18 +0530

    Module02 - lecture04 : var vs let vs const

---
+++ React Course/Module-02/lecture04.js
@@ -0,0 +1,23 @@
+// var vs let vs const 
+
+//  var
+// var a = 10;
+// var a = 20;
+
+// console.log(a);
+
+// // let
+// let a = 30;
+// let a = 20;
+
+// console.log(a);
+
+
+// const 
+
+// const c = 3.14;
+//  c = 212;
+
+// console.log(c);
+
+
