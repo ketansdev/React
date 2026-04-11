3f1918281db598db1312e1fb15c5cb878043ecd0
Ketan Shetge <ketanshetgedev@gmail.com>
2026-04-11 10:07:32 +0530

    update lecture04

--- React Course/Module-02/lecture04.js
+++ React Course/Module-02/lecture04.js
@@ -1,4 +1,4 @@
-// var vs let vs const 
+// var vs let vs const
 
 //  var
 // var a = 10;
@@ -12,12 +12,18 @@
 
 // console.log(a);
 
-
-// const 
+// const
 
 // const c = 3.14;
 //  c = 212;
 
 // console.log(c);
 
+
+
+for(var i = 1; i <= 3; i++){
+    setTimeout(() =>{
+        console.log(i);
+    }, 1000);
+}
 
