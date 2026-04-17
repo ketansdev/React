f4bb1fbca9d2cc1c01f57f0378c892676e592562
Ketan Shetge <ketanshetgedev@gmail.com>
2026-04-18 05:04:51 +0530

    Module02 - lecture17 : Boolean in JS

---
+++ React Course/Module-02/lecture17.js
@@ -0,0 +1,13 @@
+if(0) console.log("Wont Run");
+if(null) console.log("Wont Run");
+if(undefined) console.log("Wont Run");
+if("") console.log("Wont Run");
+if(false) console.log("oOnt Run");
+if(NaN) console.log("Wont Run");
+
+
+if(10) console.log("Run");
+if("Hello") console.log("Run");
+if(true) console.log("Run");
+if([]) console.log("Run");
+if({}) console.log("Run");
