01b7d537cc0de909079e29a86a0ac5a9a613c708
Ketan Shetge <ketanshetgedev@gmail.com>
2026-04-25 05:29:08 +0530

    Es6 modules

---
+++ React Course/Module-02/lecture24.js
@@ -0,0 +1,12 @@
+export const add = (a, b) => a + b;
+export const multiply = (a, b) => a * b;
+
+
+const subtract = (a, b) => a - b;
+
+export {subtract}
+
+
+export default function greet(name){
+    return `Hello ${name}`;
+}
---
+++ React Course/Module-02/main.js
@@ -0,0 +1,9 @@
+import {add, multiply, subtract as minus} from './lecture24.js'
+import greet from "./lecture24.js"
+
+console.log(add(2,5));
+console.log(multiply(5,2))
+
+console.log(minus(5, 2))
+
+console.log(greet("Ketan"))
