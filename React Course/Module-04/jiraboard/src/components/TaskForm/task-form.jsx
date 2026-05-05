8efeb97b84c5d8d2fc1342c62c530bb83d1b0d30
Ketan Shetge <ketanshetgedev@gmail.com>
2026-05-05 18:34:05 +0530

    add change event on input and select dropdown - Jiraboard

--- React Course/Module-04/jiraboard/src/components/TaskForm/task-form.jsx
+++ React Course/Module-04/jiraboard/src/components/TaskForm/task-form.jsx
@@ -1,24 +1,56 @@
 import "./task-form.css";
 import Tag from "../Tag/Tag";
+import { useState } from "react";
 
 const TaskForm = () => {
+  const [taskData, setTaskData] = useState({
+    "task": "",
+    "status": "Ready for Development"
+  })
+
+
+  const handleChange = (e) =>{
+      const {name, value} = e.target;
+      setTaskData((prev) => {
+        return {...prev, [name]:value}
+      });
+  };
+
+  const handleClick = (e) =>{
+    e.preventDefault();
+    console.log(taskData);
+  }
+
+
+  
+
   return (
     <header className="app_header">
-      <form>
+      <form onSubmit={handleClick}>
         <input
           className="form-input"
           type="text"
+          name = "task"
           placeholder="Enter your task !!"
+          onChange={handleChange}
+          
         />
         <div className="task_form_bottom">
           <div className="tags">
-            <Tag tagName = "DEV"/>
-            <Tag tagName = "QA"/>
-            <Tag tagName = "Product Owner"/>
+            <Tag tagName="DEV" />
+            <Tag tagName="QA" />
+            <Tag tagName="Product Owner" />
           </div>
 
           <div className="select_and_add">
-            <select name="" id="" className="task_options">
+            <select
+              name=""
+              id=""
+              className="task_options"
+              name = "status"
+              onChange={handleChange}
+              
+            >
               <option value="Ready for Development">
                 Ready for Development
               </option>
@@ -26,7 +58,9 @@ const TaskForm = () => {
               <option value="Ready for Test">Ready for Test</option>
               <option value="Closed">Closed</option>
             </select>
-            <button type="submit" className="add_btn">+ Add</button>
+            <button type="submit" className="add_btn" >
+              + Add
+            </button>
           </div>
         </div>
       </form>
