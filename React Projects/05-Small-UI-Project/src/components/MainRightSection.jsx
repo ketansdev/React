46ae054d795d1b580a85d0a104c80fb8eb8cffd1
Ketan Shetge <ketanshetgedev@gmail.com>
2026-05-24 18:04:13 +0530

    added bg for card buttons

--- React Projects/05-Small-UI-Project/src/components/MainRightSection.jsx
+++ React Projects/05-Small-UI-Project/src/components/MainRightSection.jsx
@@ -17,7 +17,7 @@ const MainRightSection = () => {
                 <span className="flex h-12 w-12 items-center justify-center rounded-full bg-white text-black text-3xl">{index + 1}</span>
                 <div>
                     <p className="text-2xl mb-15 text-white w-65 tracking-[4px]">{item.description}</p>
-                    <button className="flex justify-center items-center gap-10 text-2xl capitalize text-white">{item.tag} <MoveRight size={45}/></button>
+                    <button style={{backgroundColor:item.btnBg}} className="flex justify-center items-center gap-10 text-2xl capitalize text-white px-2 py-1 rounded-2xl">{item.tag} <MoveRight size={45}/></button>
                 </div>
             </div>
         </div>
--- React Projects/05-Small-UI-Project/src/constants/common.js
+++ React Projects/05-Small-UI-Project/src/constants/common.js
@@ -2,17 +2,20 @@ let array = [
     {
         img: "https://images.pexels.com/photos/6045939/pexels-photo-6045939.jpeg?_gl=1*wv7bxh*_ga*NTI0NjIwNDc2LjE3NzI2MDcxNjQ.*_ga_8JE65Q40S6*czE3NzkzNzkzNjMkbzMkZzEkdDE3NzkzNzkzNjUkajU4JGwwJGgw",
         description: "Prime customers, that have access to bank credit and are satisfied with the current product",
-        tag : "satisfied"
+        tag : "satisfied",
+        btnBg : "#506AD4"
     },
     {
         img: "https://images.pexels.com/photos/8938259/pexels-photo-8938259.jpeg?_gl=1*941vpu*_ga*NTI0NjIwNDc2LjE3NzI2MDcxNjQ.*_ga_8JE65Q40S6*czE3NzkzNzkzNjMkbzMkZzEkdDE3NzkzNzk0ODckajU5JGwwJGgw",
         description: "Prime customers, that have access to bank credit and are not satisfied with the current service",
-        tag : "undeserved"
+        tag : "undeserved",
+        btnBg : "#506AD4"
     },
     {
         img: "https://images.pexels.com/photos/5061275/pexels-photo-5061275.jpeg?_gl=1*104srsr*_ga*NTI0NjIwNDc2LjE3NzI2MDcxNjQ.*_ga_8JE65Q40S6*czE3NzkzNzkzNjMkbzMkZzEkdDE3NzkzNzk1NTYkajU5JGwwJGgw",
         description: "Customers from near-prime and sub-prime segments with no access to bank credit",
-        tag : "underbamked"
+        tag : "underbanked",
+        btnBg : "#BCE362"
     }
 ]
 
